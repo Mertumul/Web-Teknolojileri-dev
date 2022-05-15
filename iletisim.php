317f98dbc1fdbaf5d71522f611a97839884fbc46
Mert Umul <84383448+Mertumul@users.noreply.github.com>
2022-05-15 20:46:11 +0300

    Add files via upload

---
+++ iletisim.php
@@ -0,0 +1,39 @@
+<table>
+			<tr>
+				<th scope="row">İsim:</th>
+				<td><?php echo $_POST["adı"]?></td>
+			</tr>
+			<tr>
+				<th scope="row">Soyisim:</th>
+				<td><?php echo $_POST["soyadı"]?></td>
+			</tr>
+			
+			<tr>
+				<th scope="row">Cinsiyet:</th>
+				<td><?php echo $_POST["cinsiyet"]?></td>
+			</tr>
+			<tr>
+				<th scope="row">Hesaplar</th>
+				<td><?php echo $_POST["sm4"]?></td>
+			</tr> 
+            <tr>
+				<th scope="row">Email:</th>
+				<td><?php echo $_POST["email"]?></td>
+			</tr>
+            <tr>
+				<th scope="row">Okul:</th>
+				<td><?php echo $_POST["okul"]?></td>
+			</tr>
+			<tr>
+			<th scope="row">öğrencino:</th>
+				<td><?php echo $_POST["öğrencino"]?></td>
+			</tr>
+      		<tr>
+			  <th scope="row">Telefon Numarası:</th>
+				<td><?php echo $_POST["telno"]?></td>
+			</tr>
+            <tr>
+			  <th scope="row">Konu::</th>
+				<td><?php echo $_POST["konu"]?></td>
+			</tr>
+	</table>
